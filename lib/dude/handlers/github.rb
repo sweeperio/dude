5b364ac14e0a4dadac84d7504dcfd06e6f83a3a6
pseudomuto <david.muto@gmail.com>
2015-08-18 00:17:18 +0000

    triggering event from handler

--- lib/dude/handlers/github.rb
+++ lib/dude/handlers/github.rb
@@ -1,21 +1,26 @@
 class Dude::Handlers::Github < Lita::Handler
   http.post "/github/webhooks", :webhook
 
+  on :github_deploy, :start_deploy
+
   def webhook(request, response)
     case request.env['HTTP_X_GITHUB_EVENT']
     when "deployment"
-      announce("Deployment received")
+      robot.trigger(:github_deploy, "Deployment received")
     end
 
-    announce(request.body)
+    robot.trigger(:github_deploy, "received GH notification")
+  end
+
+  def start_deploy(message)
+    room   = Lita::Room.find_by_name("general")
+    target = Lita::Source.new(room: room)
+    robot.send_message(target, message)
   end
 
   private
 
   def announce(message)
-    room   = Lita::Room.find_by_name("general")
-    target = Lita::Source.new(room: room)
-    robot.send_message(target, message)
   end
 end
 
